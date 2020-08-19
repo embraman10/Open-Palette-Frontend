bdc8c77062f20463bd6a41e101c14f4af4ea981c
J-Waz <jgw.jakegeorge@gmail.com>
2020-08-18 19:24:09 -0500

    create palette joiner, post method on palletes working, rendering saved palettes working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 let colorHexes = [];
-let fontFamilies = ["Arial", "Times New Roman", "Verdana", "Helvetica", "Georgia", "Garamond", "Courier", "Impact", "Perpetua", "Cursive"];
+let fontFamilies = [
+  "Arial",
+  "Times New Roman",
+  "Verdana",
+  "Helvetica",
+  "Georgia",
+  "Garamond",
+  "Courier",
+  "Impact",
+  "Perpetua",
+  "Cursive",
+];
+document.addEventListener("click", handleSave);
+
 
 
 function main() {
   fetchColors();
   fetchFontColors();
   fetchFonts();
+  fetchPalettes();
 
 }
 
@@ -20,11 +34,11 @@ function fetchColors() {
       const btn = document.getElementById("color-btn");
       const color = document.querySelector(".color");
 
-      const div = document.getElementById("cb")
+      const div = document.getElementById("cb");
 
       btn.addEventListener("click", function () {
         const randomNumber = getRandomNumber();
-        const paletteForm = document.getElementById('palette-form')
+        const paletteForm = document.getElementById("palette-form");
         // console.log(randomNumber);
         //console.log(cb)
         div.style.backgroundColor = colorHexes[randomNumber];
@@ -38,8 +52,6 @@ function fetchColors() {
     });
 }
 
-
-
 function fetchFontColors() {
   fetch("http://localhost:3000/background_color_changers")
     .then((resp) => resp.json())
@@ -50,18 +62,17 @@ function fetchFontColors() {
       //console.log(colorHexes);
       const btn = document.getElementById("font-color-btn");
       const fontColor = document.querySelector(".font-color");
-      const h2 = document.getElementById("tester")
+      const h2 = document.getElementById("tester");
 
       btn.addEventListener("click", function () {
         const randomNumber = getRandomNumber();
         // console.log(randomNumber);
-        console.log(cb)
-        const paletteForm = document.getElementById('palette-form')
+        console.log(cb);
+        const paletteForm = document.getElementById("palette-form");
         h2.style.color = colorHexes[randomNumber];
         fontColor.textContent = colorHexes[randomNumber];
         paletteForm.fontColor.value = colorHexes[randomNumber];
       });
-
 
       function getRandomNumber() {
         return Math.floor(Math.random() * colorHexes.length);
@@ -69,38 +80,82 @@ function fetchFontColors() {
     });
 }
 
-
 function fetchFonts() {
-  // fetch("http://localhost:3000/font_changers")
-  //   .then((resp) => resp.json())
-  //   .then((fonts) => {
-  //     fonts.forEach((font) => {
-  //       fontFamilies.push(font.fontFamily);
-  //     });
-      //console.log(fontFamilies);
-      const btn = document.getElementById("font-family-btn");
-      const font = document.querySelector(".font");
-      const h2 = document.getElementsByClassName("card-inner")
 
-      
-      btn.addEventListener("click", function () {
-        const randomNumber = getRandomNumber();
-        console.log(cb)
-        // console.log(randomNumber);
-        const paletteForm = document.getElementById('palette-form')
+  const btn = document.getElementById("font-family-btn");
+  const font = document.querySelector(".font");
+  const h2 = document.getElementsByClassName("card-inner");
 
+  btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+    console.log(cb);
+    // console.log(randomNumber);
+    const paletteForm = document.getElementById("palette-form");
 
-        h2[0].style.fontFamily = fontFamilies[randomNumber];
-        paletteForm.font.value = fontFamilies[randomNumber];
-        //console.log(h2[0])
+    h2[0].style.fontFamily = fontFamilies[randomNumber];
+    paletteForm.font.value = fontFamilies[randomNumber];
+    //console.log(h2[0])
 
-        //font.textContent = fontFamilies[randomNumber];
-      });
+    //font.textContent = fontFamilies[randomNumber];
+  });
 
-      function getRandomNumber() {
-        return Math.floor(Math.random() * fontFamilies.length);
-      }
-    // });
+  function getRandomNumber() {
+    return Math.floor(Math.random() * fontFamilies.length);
+  }
+  
 }
-main();
+function fetchPalettes() {
+  fetch("http://localhost:4000/Palettes")
+    .then((resp) => resp.json())
+    .then((palletes) => {
+      palletes.forEach(renderPalletes);
+    });
+}
 
+function renderPalletes(pallete) {
+  const footer = document.getElementById("show-body");
+  footer.innerHTML += `     <div class="saved-card-body" data-id="${pallete.id}" style="background-color: ${pallete.background_color}">
+                            <h2 data-id="${pallete.id}" style="font-family: ${pallete.font_family}; color: ${pallete.font_color}">Tester Here</h2>
+                            <div>
+                            <h2> background color : <span class="color">${pallete.background_color}</span></h2>
+                            </div>
+                            <div>
+                            <h2> font color : <span class="font-color">${pallete.font_color}</span></h2>
+                            </div>
+                            <div>
+                            <h2> font : <span class="font-family">${pallete.font_family}</span></h2>
+                            </div>
+                            </div>`;
+}
+
+function handleSave(e){
+  e.preventDefault();
+  if (e.target.id === "save-btn"){
+    console.log('I am the save button')
+    submitPalette(e)
+  }
+}
+
+function submitPalette(e){
+  const paletteForm = document.getElementById("palette-form");
+  let newPallete = {
+    background_color: paletteForm.bgColor.value,
+    font_color: paletteForm.fontColor.value,
+    font_family: paletteForm.font.value
+  }
+  //console.log(newPallete)
+  fetch("http://localhost:4000/Palettes", {
+    method: 'POST',
+    headers: {"content-type": 'application/json',
+            accepts: 'application/json'
+},
+body: JSON.stringify(newPallete)
+
+})
+.then(res => res.json())
+.then(pallete => {
+  renderPalletes(pallete)
+})
+}
+
+main();
